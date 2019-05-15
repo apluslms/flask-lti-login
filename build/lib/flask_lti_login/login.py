7d1f8cfee63178c538783f9446d5d8d99f0a5270
Ruiyang Ding <dry950526@gmail.com>
2019-05-15 12:52:13 +0300

    push to aplus

---
+++ build/lib/flask_lti_login/login.py
@@ -0,0 +1,52 @@
+import logging
+
+from flask import current_app
+from flask_login import LoginManager
+
+from flask_lti_login.models import User
+
+login_manager = LoginManager()
+logger = logging.getLogger('ltilogin.login')
+
+
+def check_user_id(user_id, guid):
+    return user_id if '@' in user_id else user_id + "@" + guid
+
+
+def load_user_from_request(oauth_request):
+    body = oauth_request.body
+    if not oauth_request:
+        logger.warning('No request')
+        return None
+    try:
+        user_id = body['user_id'][:current_app.config['USER_NAME_LENGTH']]
+    except KeyError:
+        logger.warning('LTI login attempt without a user id.')
+        return None
+
+    accepted_roles = None
+    staff_roles = None
+    # get parameters and truncate to lengths that can be stored into database
+    email = body['lis_person_contact_email_primary'][:current_app.config['EMAIL_LENGTH']] or ''
+    display_name = body['lis_person_name_given'][:current_app.config['FIRST_NAME_LENGTH']] or ''
+    sorting_name = body['lis_person_name_family'][:current_app.config['LAST_NAME_LENGTH']] or ''
+    full_name = body['lis_person_name_full'][
+                :(current_app.config['FIRST_NAME_LENGTH'] + current_app.config['LAST_NAME_LENGTH'])] or ' '
+    roles = frozenset(body['roles'].split(',')) if oauth_request.body['roles'] else frozenset()
+    if accepted_roles and roles.isdisjoint(accepted_roles):
+        logger.warning('LTI login attempt without accepted user role: %s', roles)
+        return None
+    # Retrieve user information
+    print(check_user_id(user_id, body['tool_consumer_instance_guid']))
+    user = User(
+        user_id=check_user_id(user_id, body['tool_consumer_instance_guid']),
+        email=email,
+        display_name=display_name,
+        sorting_name=sorting_name,
+        full_name=full_name
+    )
+    # user.is_staff = staff_roles and not roles.isdisjoint(staff_roles) or False
+    logger.info('LTI authentication accepted for: %s', user)
+    oauth_request.redirect_url = current_app.config['LOGIN_REDIRECT_URL']
+    # oauth_request.set_cookies = []
+    return user
---
+++ build/lib/flask_lti_login/models.py
@@ -0,0 +1,18 @@
+from collections import namedtuple
+
+
+'''
+Mapping from LTI:
+lis_person_name_family -> sorting
+lis_person_name_full -> full_name
+lis_person_name_given -> display_name
+lis_person_contact_email_primary -> email
+
+if '@' not in user_id:
+ user_id = user_id "@" + tool_consumer_instance_guid
+
+'course': (context_label, context_title, context_id)
+
+query parameter 'next' -> redirect_url
+'''
+User = namedtuple('User', ['user_id', 'full_name', 'display_name', 'sorting_name', 'email'])

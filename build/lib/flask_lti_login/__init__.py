a0d98098c5371bca93c8a3b59d7ba56c417f54ee
Ruiyang Ding <dry950526@gmail.com>
2019-05-15 12:53:23 +0300

    push to aplus

--- build/lib/flask_lti_login/__init__.py
+++
@@ -1,2 +0,0 @@
-from .views import lti
-from .signals import lti_login_authenticated
--- build/lib/flask_lti_login/signals.py
+++
@@ -1,5 +0,0 @@
-from blinker import Namespace
-
-signals = Namespace()
-
-lti_login_authenticated = signals.signal('lti-login-authenticated')
